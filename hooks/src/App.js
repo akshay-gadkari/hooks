0c049ad434b04be7bbe48611aabf53a97b587e35
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-11-20 12:59:05 -0800

    console logs render

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import {useForm} from './useForm';
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+    firstName: ''
+  });
+
+  useEffect(() => {
+    console.log("render");
+  }, [values.email, values.password]); //does a shallow comparison of the values
+
+//useEffect can replace componentDidMount
+  
   return (
     <div className="App">
-      <header className="App-header">
-          Hooks
-      </header>
+      <input
+        name="email"
+        type="text"
+        value={values.email}
+        placeholder="email"
+        onChange={handleChange}
+        />
+      <input
+        name="firstName"
+        type="text"
+        value={values.firstName}
+        placeholder="first name"
+        onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={values.password}
+          placeholder="password"          
+          onChange={handleChange}
+          />
     </div>
   );
-}
+};
 
 export default App;
