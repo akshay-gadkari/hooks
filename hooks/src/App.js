87ace08328a750e82130530448658aa847425513
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-11-25 10:06:56 -0800

    modifications

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,39 +1,63 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  let note = '';
-  let notes = [];
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    notes.push(note);
-    console.log(notes);
-  };
+class App extends React.Component {
+  // let note = '';
+  // let notes = ['1'];
   
-  return (
-    <div className="App">
-      <form>
-        Add Note:
-        <br/>
-        <textarea
-          name="note"
-          placeholder="note"
-          id="myForm"
-          type="text"
-          onChange={e => note = (e.target.value)}
-          value={notes[0]}/>
+  // const DisplayNotes = () => {
+  //   if (notes.length !==0) {
+  //     return (
+  //       <div>
+  //         <h1>{notes[0]}</h1>
+  //       </div>
+  //     );
+  //   }
+  //   return null;
+  // };
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.state.list = ['a'];
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+      e.preventDefault();
+      this.state.list.push(this.state.value);
+      console.log(this.state.list);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          Add Note:
           <br/>
-          <button
-            id="myButton"
-            onClick={
-              (e) => handleSubmit(e)
-            }>
-            Submit
-          </button>
-      </form>
-      <h1>{notes}</h1>
-    </div>
-  );
+          <input
+            name="note"
+            placeholder="note"
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.value}/>
+            <br/>
+            <button
+              id="myButton"
+              onClick={
+                (e) => this.handleSubmit(e) && console.log('display')
+              }>
+              Submit
+            </button>
+        </form>
+        <ul>{this.state.list.map(value => <li>{value}</li>)}</ul>
+        </div>
+    );
+  }
 };
 
 export default App;
