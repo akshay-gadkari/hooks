2b374e11f2d98aed08147ccc2cda83e513687097
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-11-25 15:47:42 -0800

    converted into function using hooks

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,63 +1,82 @@
 import React, { useState } from 'react';
 
-class App extends React.Component {
-  // let note = '';
-  // let notes = ['1'];
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ''};
+//     this.state.list = ['a'];
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(e) {
+//     this.setState({value: e.target.value});
+//   }
+
+//   handleSubmit(e) {
+//       e.preventDefault();
+//       this.state.list.push(this.state.value);
+//       console.log(this.state.list);
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <form>
+//           Add Note:
+//           <br/>
+//           <input
+//             name="note"
+//             placeholder="note"
+//             type="text"
+//             onChange={this.handleChange}
+//             value={this.state.value}/>
+//             <br/>
+//             <button
+//               id="myButton"
+//               onClick={
+//                 (e) => this.handleSubmit(e) && console.log('display')
+//               }>
+//               Submit
+//             </button>
+//         </form>
+//         <ul>{this.state.list.map(value => <li>{value}</li>)}</ul>
+//         </div>
+//     );
+//   }
+// };
+
+// export default App;
+
+export default function App(props) {
+  const [name, setName] = useState('');
+  const [names, setNames] = useState([]);
   
-  // const DisplayNotes = () => {
-  //   if (notes.length !==0) {
-  //     return (
-  //       <div>
-  //         <h1>{notes[0]}</h1>
-  //       </div>
-  //     );
-  //   }
-  //   return null;
-  // };
-
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-    this.state.list = ['a'];
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({value: e.target.value});
-  }
-
-  handleSubmit(e) {
-      e.preventDefault();
-      this.state.list.push(this.state.value);
-      console.log(this.state.list);
+  const handleSubmit = e => {
+    e.preventDefault();
+    setNames([
+      ...names,
+      name
+    ]);
   };
-
-  render() {
-    return (
-      <div className="App">
-        <form>
-          Add Note:
-          <br/>
-          <input
-            name="note"
-            placeholder="note"
-            type="text"
-            onChange={this.handleChange}
-            value={this.state.value}/>
-            <br/>
-            <button
-              id="myButton"
-              onClick={
-                (e) => this.handleSubmit(e) && console.log('display')
-              }>
-              Submit
-            </button>
-        </form>
-        <ul>{this.state.list.map(value => <li>{value}</li>)}</ul>
-        </div>
-    );
-  }
+  
+  return (
+    <div className="App">
+      <form>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="name"/>
+          <button onClick={handleSubmit}>Add a name</button>
+      </form>
+      <ul>
+        {names.map(name => (
+          <li>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-
-export default App;
