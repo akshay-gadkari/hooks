5adb6dad397801221e7bf4a295266decee42ac80
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-12-03 19:54:46 -0800

    minor changes for strikethrough

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,15 +2,19 @@ import React, {useReducer, useState} from "react";
 
 function reducer(state, action) { //pure function, so it's outside the App function
   switch(action.type) {
-  case 'INCREMENT':
-    return state + 1;
-  case 'DECREMENT':
-    return state - 1;
+  case 'add-todo':
+    return {
+      todos: [...state.todos, {text: action.text, completed: false}]
+    };
+  case 'toggle-todo':
+    return {
+      todos: state.todos.map((t, idx) => idx === action.idx ? {...t, completed: !t.completed} : t)
+    };
   default:
     return state;
   }
 }
-
+ 
 const App = () => {
   const [{todos}, dispatch] = useReducer(reducer, {todos: []});
   const [text, setText] = useState();
@@ -23,9 +27,20 @@ const App = () => {
         }}>
         <input placeholder="todo" type="text" value={text} onChange={e => setText(e.target.value)}/>
       </form>
-      <pre>
+      {todos.map((t, idx) => (
+        <div
+          key={t.text}
+          onCLick={() => dispatch({type: 'toggle-todo', idx})}
+          style={{
+            textDecoration: t.completed ? 'line-through' : ''
+          }}
+          >
+          {t.text}
+        </div>
+      ))}
+    <pre>
         {JSON.stringify(todos, null, 2)}
-      </pre>
+        </pre>
     </div>
   );
 };
