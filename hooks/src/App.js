cdfbc5bd057584b32279db3d24c0a54c8c8992f1
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-12-06 14:45:20 -0800

    initial commit

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,48 +1,28 @@
-import React, {useReducer, useState} from "react";
+import React, {useReducer, useState, useContext} from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Index } from "./pages";
+import { About } from "./pages/about";
 
-function reducer(state, action) { //pure function, so it's outside the App function
-  switch(action.type) {
-  case 'add-todo':
-    return {
-      todos: [...state.todos, {text: action.text, completed: false}]
-    };
-  case 'toggle-todo':
-    return {
-      todos: state.todos.map((t, idx) => idx === action.idx ? {...t, completed: !t.completed} : t)
-    };
-  default:
-    return state;
-  }
-}
+function AppRouter() { //pure function, so it's outside the App function
 
-const App = () => {
-  const [{todos}, dispatch] = useReducer(reducer, {todos: []});
-  const [text, setText] = useState();
   return (
-    <div>
-      <form onSubmit={e => {
-          e.preventDefault();
-          dispatch({type: 'add-todo', text});
-          setText('');
-        }}>
-        <input placeholder="todo" type="text" value={text} onChange={e => setText(e.target.value)}/>
-      </form>
-      {todos.map((t, idx) => (
-        <div
-          key={t.text}
-          onClick={() => dispatch({type: 'toggle-todo', idx})}
-          style={{
-            textDecoration: t.completed ? "line-through" : ""
-          }}
-          >
-          {t.text}
-        </div>
-      ))}
-    {/* }<pre>
-        {JSON.stringify(todos, null, 2)}
-        </pre> */}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" component={Index}/>
+        <Route path="/about/" component={About}/>
+      </div>
+    </Router>
   );
 };
 
-export default App;
+export default AppRouter;
